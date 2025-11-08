536b34e2ef2da263d24c3559dffe874262ecc915
stecape <ste.cape@gmail.com>
2025-11-08 22:02:59 +0100

    gestione docker compose con nginx

--- client/src/Helpers/socket.jsx
+++ client/src/Helpers/socket.jsx
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 import React from 'react'
 
-const serverIp = process.env.REACT_APP_SERVER_IP || "http://localhost:3001"
+const serverIp = process.env.REACT_APP_SERVER_IP || "http://localhost"
 export const socket = io(serverIp)
 export const SocketContext = React.createContext()
